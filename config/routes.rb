8d3dec365a5d8b8062d29f85870262ef4d28fcae
Guilherme Arruda <guilherme_arruda@live.com>
2019-12-29 19:38:34 -0300

    Criação dos controllers

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,7 @@
 Rails.application.routes.draw do
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  get '/categories', to: 'categories#index'
+  get '/restaurants', to: 'restaurants#index'
+  get '/restaurants/search', to: 'restaurants#search'
+  post '/orders', to: 'orders#create'
+  get '/orders/:id', to: 'categories#index'
 end
